bd3453bc9522c744792e1ec5698d7a206829d6c3
Alex Kuzmenya <alex.kuzmenya@gmail.com>
2016-04-28 20:34:41 +0300

    Ангулар компонент quantity-input

---
+++ frontend/src/components/quantity-input/quantity-input.js
@@ -0,0 +1,19 @@
+angular.module('Foodhub')
+  .controller('quantityController', ['$scope', function($scope) {
+    $scope.counter = 1;
+    $scope.decreaseCount = function(){
+      if ($scope.counter > 0){
+        $scope.counter -= 1;
+      }
+    };
+    $scope.increaseCount = function(){
+      $scope.counter += 1;
+    };
+  }])
+  .directive('quantityInput', function () {
+    return {
+      template: require('./quantity-input.html'),
+      replace: true,
+      restrict: 'E'
+    };
+  });
