0ae94d24b1dbf85d55aff985662491e8b194fce1
Alex Kuzmenya <alex.kuzmenya@gmail.com>
2016-04-29 07:05:30 +0300

    Внес контролер внутрь, добавил scope

--- frontend/src/components/quantity-input/quantity-input.js
+++ frontend/src/components/quantity-input/quantity-input.js
@@ -1,19 +1,25 @@
 angular.module('Foodhub')
-  .controller('quantityController', ['$scope', function ($scope) {
-    $scope.counter = 1;
-    $scope.decreaseCount = function () {
-      if ($scope.counter > 0) {
-        $scope.counter -= 1;
-      }
-    };
-    $scope.increaseCount = function () {
-      $scope.counter += 1;
-    };
-  }])
   .directive('quantityInput', function () {
     return {
       template: require('./quantity-input.html'),
       replace: true,
       restrict: 'E',
+      scope: {
+        foodCounter: '='
+      },
+      controller: function ($scope) {
+        //делаем foodCounter равным нулю, если внешняя переменная не задана
+        if(typeof $scope.foodCounter == "undefined") {
+          $scope.foodCounter = 0;
+        }
+        $scope.increaseCount = function () {
+          $scope.foodCounter += 1;
+        };
+        $scope.decreaseCount = function () {
+          if ($scope.foodCounter > 0) {
+            $scope.foodCounter -= 1;
+          }
+        };
+      },
     };
   });
