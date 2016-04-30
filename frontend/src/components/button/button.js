bb278ccc665574290129d4e4138394ae2a774a23
dmowski <dmowski@yandex.ru>
2016-04-30 09:58:15 +0300

    исправления по кнопке

--- frontend/src/components/button/button.js
+++ frontend/src/components/button/button.js
@@ -4,14 +4,35 @@ angular.module('Foodhub').component('customButton', {
     bindings: {
        'title': '@',
        'href': '@',
-       'target': '@',
+       'isBlank': '@',
        'isStretch' : '@',
-       "isTransparent": "@",
+       'isTransparent': '@',
+       'clickAction' : '=',
     },
 
     template: require('./button.html'),
 
     controller: function() {
+
+      this.isTransparentBtn = function () {
+        return (this.isTransparent && this.isTransparent != 0);
+      }
+
+      this.isStretchBtn = function () {
+        return (this.isStretch && this.isStretch != 0);
+      }
+
+      this.getTargetData = function () {
+        return (this.isBlank) ? "_blank" : "_self";
+      }
+
+
+      this.click_btn = function ($event) {
+        if(this.clickAction){
+          $event.preventDefault() 
+          this.clickAction()
+        }
+      }
 
     }
 });
