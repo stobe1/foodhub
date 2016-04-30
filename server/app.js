667820674ae3029f26d64b87121df782a81c0a84
Aleksey Bartashevich <alexey.barton@gmail.com>
2016-04-30 18:52:18 +0300

    создал middleware

--- server/app.js
+++ server/app.js
@@ -9,6 +9,12 @@ var routes = require('./routes/routes');
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
