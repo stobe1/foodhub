14ce2a85e1e71be844093ee4245ce6c8e3da9f8e
ant478 <ant478@gmail.com>
2016-05-07 20:17:52 +0300

    Добавил экшена контроллера sessions, добавил связи между моделями

--- server/controllers/sessionsController.js
+++ server/controllers/sessionsController.js
@@ -1,15 +1,95 @@
-exports.index = function(request, response) {
-  response.send('Sessions#index');
+var errors = require('../errors/errors');
+var models = require('../models/models');
+var _ = require('lodash');
+
+var shortQueryOptions = {
+  attributes: {
+    exclude: ['createdAt', 'updatedAt', 'userId']
+  },
+  include: {
+    model: models.User,
+    as: 'owner',
+    attributes: ['id', 'firstName', 'lastName']
+  }
 }
 
-exports.show = function(request, response) {
-  response.send('Sessions#show');
+var fullQueryOptions = {
+  attributes: {
+    exclude: ['createdAt', 'updatedAt', 'userId']
+  },
+  include: [{
+    model: models.User,
+    as: 'owner',
+    attributes: ['id', 'firstName', 'lastName']
+  }, {
+    model: models.Order,
+    as: 'orders',
+    attributes: ['id', 'isPayed', 'price'],
+    include: [{
+      model: models.User,
+      as: 'owner',
+      attributes: ['id', 'firstName', 'lastName']
+    }, {
+      model: models.FoodOrder,
+      as: 'foodOrders',
+      attributes: ['id', 'price', 'quantity'],
+      include: {
+        model: models.Food,
+        as: 'food',
+        attributes: ['id', 'name', 'description', 'imageUrl', 'price']
+      }
+    }]
+  }]
 }
 
-exports.create = function(request, response) {
-  response.send('Sessions#create');
+var createdFields = ['shopId', 'orderTime', 'address'];
+var updatedFields = ['orderTime', 'deliveryTime', 'address', 'status'];
+
+exports.index = function(request, response, next) {
+  models.Session.all(shortQueryOptions).then(function(sessions) {
+    response.status(200).json({ sessions: sessions });
+  });
 }
 
-exports.update = function(request, response) {
-  response.send('Sessions#update');
+exports.show = function(request, response, next) {
+  models.Session.findById(request.params.id, fullQueryOptions).then(function(session) {
+    if (session) {
+      response.status(200).json(session);
+    } else {
+      return next(new errors.notFound('Session not found'));
+    }
+  });
+}
+
+exports.create = function(request, response, next) {
+  var sessionOptions = _.pick(request.body, createdFields);
+  sessionOptions.userId = request.user.id;
+  models.Session.create(sessionOptions).then(function(session) {
+    models.Session.findById(session.id, fullQueryOptions).then(function(session) {
+      response.status(201).json(session);
+    });
+  }).catch(function(error) {
+    return next(new errors.badRequest(error.message));
+  });
+}
+
+exports.update = function(request, response, next) {
+  models.Session.findById(request.body.id, shortQueryOptions).then(function(session) {
+    if (session) {
+      if (session.owner.id === request.user.id) {
+        return session;
+      } else {
+        return next(new errors.forbidden('You are not allowed to update this session'));
+      }
+    } else {
+      return next(new errors.notFound('Session not found'));
+    }
+  }).then(function(session) {
+    session.updateAttributes(_.pick(request.body, updatedFields));
+    return session.save();
+  }).then(function(session) {
+    response.status(200).json(session);
+  }).catch(function(error) {
+    return next(new errors.badRequest(error.message));
+  });
 }
--- server/models/models.js
+++ server/models/models.js
@@ -7,11 +7,26 @@ var Session = db.import('./Session');
 var Shop = db.import('./Shop');
 var User = db.import('./User');
 
-Shop.hasMany(FoodCategory, { foreignKey: 'shop_id', as: 'categories' });
-FoodCategory.belongsTo(Shop, { foreignKey: 'shop_id' });
+Shop.hasMany(FoodCategory, { foreignKey: 'shopId', as: 'categories' });
+FoodCategory.belongsTo(Shop, { foreignKey: 'shopId', as: 'shop' });
 
-FoodCategory.hasMany(Food, { foreignKey: 'category_id' });
-Food.belongsTo(FoodCategory, { foreignKey: 'category_id' });
+FoodCategory.hasMany(Food, { foreignKey: 'categoryId', as: 'foods' });
+Food.belongsTo(FoodCategory, { foreignKey: 'categoryId', as: 'category' });
+
+User.hasMany(Session, { foreignKey: 'userId', as: 'sessions' });
+Session.belongsTo(User, { foreignKey: 'userId', as: 'owner' });
+
+User.hasMany(Order, { foreignKey: 'userId', as: 'orders' });
+Order.belongsTo(User, { foreignKey: 'userId', as: 'owner' });
+
+Session.hasMany(Order, { foreignKey: 'sessionId', as: 'orders' });
+Order.belongsTo(Session, { foreignKey: 'sessionId', as: 'session' });
+
+Food.hasMany(FoodOrder, { foreignKey: 'foodId', as: 'foodOrders' });
+FoodOrder.belongsTo(Food, { foreignKey: 'foodId', as: 'food' });
+
+Order.hasMany(FoodOrder, { foreignKey: 'orderId', as: 'foodOrders' });
+FoodOrder.belongsTo(Order, { foreignKey: 'orderId', as: 'order' });
 
 exports.Food = Food;
 exports.FoodCategory = FoodCategory;
