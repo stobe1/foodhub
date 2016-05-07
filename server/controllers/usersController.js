6fb24f66d7a5ef8a8fce06ce1944ad8695299491
ant478 <ant478@gmail.com>
2016-05-07 13:14:01 +0300

    Подрефакторил после ревью

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,54 +1,42 @@
 var errors = require('../errors/errors');
 var db = require('../db');
 var User = db.import('../models/User');
+var _ = require('lodash');
 
 var queryOptions = {
   attributes: { exclude: ['token', 'tokenExpiresAt', 'createdAt', 'updatedAt'] }
 }
+var updatedFields = ['firstName', 'lastName', 'email', 'phone', 'paymentOption', 'address', 'avatarUrl'];
 
 exports.index = function(request, response, next) {
-  User.all(queryOptions).then(function(users){
+  User.all(queryOptions).then(function(users) {
     response.status(200).json({ users: users });
-  });  
+  });
 }
 
 exports.show = function(request, response, next) {
-  if (isNaN(Number(request.params.id))) {
-    return next(new errors.badRequest('User ID is invalid'));
-  };
   User.findById(request.params.id, queryOptions).then(function(user) {
     if (user) {
       response.status(200).json(user);
     } else {
       return next(new errors.notFound('User not found'));
-    }      
+    }
   });
 }
 
 exports.update = function(request, response, next) {
-  if (isNaN(Number(request.body.id))) {
-    return next(new errors.badRequest('User ID is invalid or missing'));
-  };
-  if (request.user.id !== Number(request.body.id)) {
-    return next(new errors.forbidden('You are not allowed to update this user'));
-  };
   User.findById(request.body.id, queryOptions).then(function(user) {
     if (!user) {
       return next(new errors.notFound('User not found'));
-    }
-    user.updateAttributes({
-      firstName: request.body.firstName,
-      lastName: request.body.lastName,
-      email: request.body.email,
-      phone: request.body.phone,
-      paymentOption: request.body.paymentOption,
-      address: request.body.address,
-      avatarUrl: request.body.avatarUrl
-    });
-    user.save().then(function(user){
-      response.status(200).json(user);
-    }).catch(function(error){
-      return next(new errors.badRequest(error.message));
-    });
-  });
+    } else {
+      return user;
+    }    
+  }).then(function(user) {
+    user.updateAttributes(_.pick(request.body, updatedFields));
+    return user.save();
+  }).then(function(user) {
+    response.status(200).json(user);
+  }).catch(function(error) {
+    return next(new errors.badRequest(error.message));
+  });;
 }
--- server/middleware/isAuthenticated.js
+++ server/middleware/isAuthenticated.js
@@ -1,6 +1,6 @@
 var errors = require('../errors/errors');
 
-module.exports = function isLogin(req, res, next){
+module.exports = function isLogin(req, res, next) {
   if(req.user) return next();
-  throw new errors.unauthorized('Access allowed only for registered users');
+  return next(new errors.unauthorized('Access allowed only for registered users'));
 };
