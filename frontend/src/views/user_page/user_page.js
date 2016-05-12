1a9c442cda5b89d131766d94238ed5841f2ed527
ant478 <ant478@gmail.com>
2016-05-12 03:30:07 +0300

    Добавил функционал страницы профиля

--- frontend/src/views/user_page/user_page.js
+++ frontend/src/views/user_page/user_page.js
@@ -1,3 +1,16 @@
 angular.module('Foodhub')
-  .controller('UserPageController', function($scope) {
-  });
+  .controller('UserPageController', ['$scope', '$filter', '$routeParams', '$rootScope', 'Users', function($scope, $filter, $routeParams, $rootScope, Users) {
+    $scope.sessionInfoTitle = 'Оформление заказа';
+
+    $scope.init = function() {
+      $scope.tempUser = _.clone($rootScope.currentUser);
+    };
+
+    $scope.saveUser = function() {
+      Users.updateUser($scope.tempUser).then(function(user) {
+        $rootScope.currentUser = user;
+      })
+    }
+
+    $scope.init();
+  }]);
