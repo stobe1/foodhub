7cde77b49d87bac94c91ecde75f1bdd71610fad5
dmowski <dmowski@yandex.ru>
2016-05-13 01:13:16 +0300

    Дополнил блок информации о сессии

--- frontend/src/components/session_list/session_list.js
+++ frontend/src/components/session_list/session_list.js
@@ -1,11 +1,28 @@
-angular.module('Foodhub')
-  .directive('sessionList', function() {
-    return {
-      template: require('./session_list.html'),
-      replace: true,
-      restrict: 'E',
-      scope: {
-        sessionList: '=list'
-      }
+'use strict';
+
+angular.module('Foodhub').component('sessionList', {
+  bindings: {
+    'list': '<'
+  },
+
+  template: require('./session_list.html'),
+
+  controller: function() {
+
+    this.isHistory = function(status) {
+      return (status == 2);
     };
-  });
+
+    this.isSent = function(status) {
+      return (status == 1);
+    };
+
+
+    this.isActive = function(status) {
+      return (status == 0);
+    };
+
+    
+
+  }
+});
