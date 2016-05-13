9404f0b229b59d18e5cfc8d75691c5f9dcd32e87
squid48 <serzhan4844@gmail.com>
2016-05-13 17:16:57 +0300

    Привязка магазинов, обернуто в cron

--- server/parser/script.js
+++ server/parser/script.js
@@ -3,70 +3,84 @@
 var PizzaTempo = require('./PizzaTempo');
 var db = require('../db');
 var models = require('../models/models');
+var CronJob = require('cron').CronJob;
 
+var parse = new CronJob('00 00 00 * * *', function() {
+  var tempo = new PizzaTempo();
+  var tempoProducts = tempo.getProducts();
 
-var tempo = new PizzaTempo();
-var tempoProducts = tempo.getProducts();
-var tempoShopId = 1;
-
-tempoProducts.then((res) => {
-  var category = {};
-  res = res.map((item) => {
-    category[item.category] = true;
-
-    return {
-      name: item.name,
-      description: item.description,
-      imageUrl: item.imageUrl,
-      price: item.price,
-      category: item.category,
-      shopId: tempoShopId,
-      externalFoodId: item.externalFoodId
-    };
-  });
-
-  category = Object.keys(category).map((item) => {
-    return {
-      name: item,
-      shopId: tempoShopId
-    };
-  });
-
-  db.transaction((t) => {
-
-    return models.FoodCategory.destroy({
-      where: {},
-      transaction: t
-    }).then(() => {
-      return models.Food.destroy({
-        where: {},
-        transaction: t
-      });
-    }).then(() => {
-      return models.FoodCategory.bulkCreate(category, {
-        transaction: t
-      });
-    }).then(() => {
-      return models.FoodCategory.findAll({
-        where: {
-          shopId: 1
-        },
-        transaction: t
-      });
-    }).then((categories) => {
+  models.Shop.findOne({
+    where: {
+      name: 'Пицца Темпо'
+    }
+  }).then((shop) => {
+    var shopId = shop.id;
+    tempoProducts.then((res) => {
+      var category = {};
       res = res.map((item) => {
-        categories.forEach((category) => {
-          if (category.name == item.category) {
-            item.categoryId = category.id;
-          }
-        });
-        delete item.category;
-        return item;
+        category[item.category] = true;
+
+        return {
+          name: item.name,
+          description: item.description,
+          imageUrl: item.imageUrl,
+          price: item.price,
+          category: item.category,
+          externalFoodId: item.externalFoodId
+        };
       });
-      return models.Food.bulkCreate(res, {
-        transaction: t
+
+      category = Object.keys(category).map((item) => {
+        return {
+          name: item,
+          shopId: shopId
+        };
+      });
+
+      db.transaction((t) => {
+
+        return models.FoodCategory.destroy({
+          where: {},
+          transaction: t
+        }).then(() => {
+          return models.Food.destroy({
+            where: {},
+            transaction: t
+          });
+        }).then(() => {
+          return models.FoodCategory.bulkCreate(category, {
+            transaction: t
+          });
+        }).then(() => {
+          return models.FoodCategory.findAll({
+            where: {
+              shopId: shopId
+            },
+            transaction: t
+          });
+        }).then((categories) => {
+          res = res.map((item) => {
+            categories.forEach((category) => {
+              if (category.name == item.category) {
+                item.categoryId = category.id;
+                item.shopId = category.shopId;
+              }
+            });
+            delete item.category;
+            return item;
+          });
+          return models.Food.bulkCreate(res, {
+            transaction: t
+          });
+        });
       });
     });
   });
-
-});
+}, function () {
+  console.log('Parsing done!');
+},
+  true,
+  null,
+  null,
+  true
+);
