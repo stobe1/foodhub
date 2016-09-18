b9211f0446c474fefe24469a8d94b224d7843977
Alex Kuzmenya <alex.kuzmenya@gmail.com>
2016-09-18 20:19:15 +0300

    changed callback for logging in

--- config/config.js
+++ config/config.js
@@ -10,12 +10,12 @@ var config = {
     facebook: {
       id: 235051813522395,
       secret: '0b6f240ce894dee5c3c0066072d5b80b',
-      callback: 'http://foodhub.com:8080/api/v1/login/facebook/return'
+      callback: 'http://foodhub-test.herokuapp.com/api/v1/login/facebook/return'
     },
     vkontakte: {
       id: 5462172,
       secret: 'Zg2XnLGA9Z8Tz1Lm30Wa',
-      callback: 'http://foodhub.com:8080/api/v1/login/vkontakte/return'
+      callback: 'http://foodhub-test.herokuapp.com//api/v1/login/vkontakte/return'
     }
   }
 };
